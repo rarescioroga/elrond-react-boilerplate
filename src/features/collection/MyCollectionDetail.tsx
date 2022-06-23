fe117c32aa7a75bd611e22a1aa36f1c4873bdf9c
Rares Cioroga <ciorogarares@gmail.com>
2022-06-23 23:15:07 +0300

    Refactor my collection detail screen

--- src/features/collection/MyCollectionDetail.tsx
+++ src/features/collection/MyCollectionDetail.tsx
@@ -7,26 +7,16 @@ import { useNavigate, useParams } from 'react-router-dom';
 import useCollections from '../../common/redux/hooks/useCollections';
 import useNft from '../../common/redux/hooks/useNft';
 import {
-    CollectionDescriptionContainer,
     CollectionPriceContainer,
+    ImageAndBenefits,
     MainButton,
     MediumLargeBoldText,
-    MediumRegularText,
     NftCard,
     SmallRegularText,
 } from '@haos-labs/tesserae-utils';
-import upArrowIcon from '../../assets/icons/up-arrow-icon.png';
 import { CollectionsGridLayout, LeftContentWrapper } from '../../common/styles/nftStyles';
 import { colorTheme } from '../../constants/colors';
 import useShop from '../../common/redux/hooks/useShop';
-
-interface ArrowIconProps {
-    inverse: boolean;
-}
-
-interface BenefitsContainerProps {
-    showExtendedContainer: boolean;
-}
 
 const Container = styled(ScreenWrapper)`
     display: flex;
@@ -36,55 +26,6 @@ const Container = styled(ScreenWrapper)`
     max-width: 1227px;
     width: 100%;
     margin-top: 70px;
-`;
-
-const ArrowIcon = styled.img<ArrowIconProps>`
-    width: 10px;
-    padding: 12px;
-    cursor: pointer;
-    transition: all 1s;
-    -webkit-transition: all 0.6s;
-    -moz-transition: all 0.6s;
-    -ms-transition: all 0.6s;
-    -o-transition: all 0.6s;
-    transform: ${({ inverse }) => inverse && 'rotate(-180deg) translateY(-10px)'};
-`;
-
-const LeftContent = styled.div`
-    display: flex;
-    flex: 1;
-    flex-direction: column;
-    margin-right: 25px;
-`;
-
-const RightContent = styled(LeftContent)`
-    flex: 2.43;
-    margin-right: 20px;
-`;
-
-const Image = styled.img`
-    width: 100%;
-    border-radius: 10px;
-    object-fit: cover;
-`;
-
-const FlexRow = styled(BaseFlexRow)`
-    align-items: center;
-    justify-content: space-between;
-    margin-bottom: 33px;
-`;
-
-const BenefitsContainer = styled.div<BenefitsContainerProps>`
-    display: flex;
-    flex-direction: column;
-    align-items: flex-start;
-    overflow: hidden;
-    transition: max-height 1s;
-    -webkit-transition: max-height 1s;
-    -moz-transition: max-height 1s;
-    -ms-transition: max-height 1s;
-    -o-transition: max-height 1s;
-    max-height: ${({ showExtendedContainer }) => (showExtendedContainer ? 1000 : 0)}px;
 `;
 
 let theme = {
@@ -99,7 +40,6 @@ const MyCollectionDetail: React.FC = () => {
     const { myNfts } = useNft(collectionId || '');
     const [collection, setCollection] = useState<any>(null);
     const { shopTheme } = useShop(collection?.shop_name);
-    const [showBenefits, setShowBenefits] = useState<boolean>(false);
 
     if (shopTheme) {
         theme = shopTheme;
@@ -122,45 +62,12 @@ const MyCollectionDetail: React.FC = () => {
     return (
         <ScreenWrapper>
             <Container>
-                <BaseFlexRow>
-                    <LeftContent>
-                        <Image src={getCollectionImageSrc(collection)} />
-                    </LeftContent>
-                    <RightContent>
-                        <CollectionDescriptionContainer
-                            title={collection.token_name}
-                            subtitle={`By ${collection.shop_name}`}
-                            detailsTitle="Description"
-                            details={collection.description}
-                            logoSrc="https://i.pinimg.com/280x280_RS/81/a7/ce/81a7ce9d3bc250bd44fae2b7f188c685.jpg"
-                            showDivider
-                        >
-                            <>
-                                <FlexRow>
-                                    <MediumLargeBoldText extraCss="margin-bottom: 12px">Benefits</MediumLargeBoldText>
-                                    <ArrowIcon
-                                        src={upArrowIcon}
-                                        onClick={() => setShowBenefits((val) => !val)}
-                                        inverse={showBenefits}
-                                    />
-                                </FlexRow>
-                                <BenefitsContainer showExtendedContainer={showBenefits}>
-                                    {showBenefits &&
-                                        collection.benefits.map((benefit: any, index: any) => (
-                                            <React.Fragment key={`benefit-${index}`}>
-                                                <MediumLargeBoldText extraCss="margin-bottom: 12px">
-                                                    {benefit.name}
-                                                </MediumLargeBoldText>
-                                                <MediumRegularText extraCss="margin-bottom: 25px;">
-                                                    {benefit.description}
-                                                </MediumRegularText>
-                                            </React.Fragment>
-                                        ))}
-                                </BenefitsContainer>
-                            </>
-                        </CollectionDescriptionContainer>
-                    </RightContent>
-                </BaseFlexRow>
+                <ImageAndBenefits
+                    imageSrc={getCollectionImageSrc(collection)}
+                    collection={collection}
+                    theme={theme}
+                    logoSrc="https://i.pinimg.com/280x280_RS/81/a7/ce/81a7ce9d3bc250bd44fae2b7f188c685.jpg"
+                />
                 <CollectionsGridLayout style={{ marginTop: 25 }}>
                     {myNfts.map((nft: any, index: any) => (
                         <NftCard
